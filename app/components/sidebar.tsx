8d09906817f098cd9d29050e49ec07b77480ef68
TenkyZhu <125980242+TenkyZhu@users.noreply.github.com>
2023-11-03 21:13:55 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,7 +145,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Tenky's ChatGPT
+          Tenky’s ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Tenky Up Up Up.
